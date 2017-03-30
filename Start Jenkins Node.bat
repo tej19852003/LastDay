9bbbd389b8c40a7f67d01be9c9be67ac7994bd8e
Tej Patel <tej19852003@gmail.com>
2017-03-30 13:34:05 -0500

    Add files via upload

---
+++ Start Jenkins Node.bat
@@ -0,0 +1,7 @@
+@ECHO off
+ECHO.
+ECHO Starting Jenkins Node "WPWD50H4" for Server "lt00nxam0090000.opr.statefarm.org"
+ECHO.
+TIMEOUT /t 5
+
+javaws http://lt00nxam0090000.opr.statefarm.org/jenkins/computer/WPWD50H4/slave-agent.jnlp
